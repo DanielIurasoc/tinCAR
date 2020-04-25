32e64b068d558e4785992a3b20b8665da79fbee1
Daniel <diurasoc@gmail.com>
2020-04-25 18:36:21 +0300

    Added JavaFX main class, added .gitignore file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
 
     }
 }
