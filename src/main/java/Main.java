8ef67313a51ad2d3a23db67d72b19053b76783cb
Fabian <jichifabian@yahoo.com>
2020-04-25 21:18:53 +0300

    Icon and Logo added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +12,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("tinCAR - The place to find your new car");
         primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.getIcons().add(new Image("icon.png"));
         primaryStage.show();
     }
 
