ac54fa958ad07cf22bc41cf1824ff6f48ae414f0
Fabian <jichifabian@yahoo.com>
2020-06-20 10:28:14 +0300

    Login controller testing with some bugs that need to be fixed

---
+++ src/test/java/AnnouncementServiceTest.java
@@ -0,0 +1,33 @@
+import Services.AnnouncementService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class AnnouncementServiceTest {
+    @Test
+    public void getAnnouncementsTest() throws IOException, ParseException {
+        JSONArray announcements = AnnouncementService.getAnnouncements("../tinCAR/src/test/resources/announcements.json");
+        JSONObject x;
+
+        x = (JSONObject) announcements.get(0);
+        assertEquals("jfabian", x.get("owner"));
+        assertEquals("0725 912 234", x.get("phone"));
+
+        x = (JSONObject) announcements.get(1);
+        assertEquals("alxionescu", x.get("owner"));
+        assertEquals("0736 562 526", x.get("phone"));
+
+        x = (JSONObject) announcements.get(2);
+        assertEquals("jfabian", x.get("owner"));
+        assertEquals("0734 159 950", x.get("phone"));
+
+        x = (JSONObject) announcements.get(3);
+        assertEquals("jfabian", x.get("owner"));
+        assertEquals("0784 500 300", x.get("phone"));
+    }
+}
---
+++ src/test/java/LoginControllerTest.java
@@ -0,0 +1,50 @@
+import Controllers.LoginController;
+import Services.UserService;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginControllerTest extends ApplicationTest {
+
+    private LoginController controller;
+
+    @BeforeClass
+    public static void setupClass() throws Exception {
+        UserService.loadUsersFromFile("../tinCAR/src/test/resources/users.json");
+    }
+
+    @Before
+    public void setUp() throws IOException, ParseException {
+        UserService.loadUsersFromFile("../tinCAR/src/test/resources/users.json");
+
+        controller = new LoginController();
+        controller.usernameField = new TextField();
+        controller.passwordField = new PasswordField();
+        controller.loginMessage = new Text();
+    }
+
+    @Test
+    public void testHandleLoginButtonWithCorrectAccount() throws IOException {
+        controller.passwordField.setText("pass2");
+        controller.usernameField.setText("jfabian");
+        controller.handleLoginButton();
+        assertEquals("Login successfully", controller.loginMessage.getText());
+    }
+
+    @Test
+    public void testHandleLoginButtonWithNonExistingAccount() throws IOException {
+        controller.passwordField.setText("testUsername");
+        controller.usernameField.setText("testPassword");
+        controller.handleLoginButton();
+        assertEquals("Username or password invalid. Please try again !", controller.loginMessage.getText());
+    }
+}
